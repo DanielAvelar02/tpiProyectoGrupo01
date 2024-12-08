8aaab565015adc0a54495ce0b57345ccdba91910
kenetcode <k2ken2424@gmail.com>
2024-12-07 20:09:34 -0600

    Resolviendo production 2

--- build.sh
+++ build.sh
@@ -9,7 +9,6 @@ pip install -r requirements.txt
 python manage.py collectstatic --no-input
 
 # Apply any outstanding database migrations
-python manage.py makemigrations
 python manage.py migrate
 
 python manage.py shell -c "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('admin', 'admin@gmail.com', 'admin')"
